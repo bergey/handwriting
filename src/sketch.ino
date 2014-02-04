860cf2ad7b9baea19fca006a03823899e2983c6d
Daniel Bergey <bergey@alum.mit.edu>
2014-02-04 19:02:53 +0000

    initial import: basically untested

---
+++ src/sketch.ino
@@ -0,0 +1,44 @@
+/*
+  chording keyboard code (c) Daniel Bergey 2014
+*/
+
+// anything above `threshold` indicates a pressed sensor
+const int threshold = 600;  // voltage divider 50/50 unpressed, 67/33 pressed
+
+/* keymap in binary order, with thumb as least-significant bit, pinkie on 0x10 */
+const char keymap[] = { ' ', // no keys pressed, do not print
+                       ' ' , 'u' , 's' , 'e' , 'n' , 'i' , 't', // 0x01-0x07 
+                       'o' , 'h' , 'r' , 'd' , 'l' , 'c' , 'm' , 'w', // 0x08-0x0F
+                       'a' , 'b' , 'v' , 'k' , 'q' , 'j' , 'x' , 'z', // 0x10-0x17
+                       0x80 /* ctrl */ , 'f' , 'g' , 'y' , 0xB1 /* 0xB1 esc*/ , 'p', 0xC1 , 0x81 /* shift */
+                       // 0x1A-0x1F
+};
+// Modifier Keys: http://arduino.cc/en/Reference/KeyboardModifiers
+
+int fingertip_state = 0;
+
+void setup() {
+     // Do not need to set input mode for analog pins
+     /* int thumbState, firstState, middleState, ringState, pinkiState; */
+     Keyboard.begin();
+}
+
+void loop() {
+     fingertip_state = 0;
+
+// wire fingertip sensors to analog inputs A0 through A4
+     // thumb on A0
+     if (digitalRead(12)) {
+          for(int i=0; i<5; i++) {
+               fingertip_state |= ((analogRead(i) > threshold ? 1 : 0) << i);
+          }
+
+          if (fingertip_state != 0x00) {
+               // only lowercase for now
+               Keyboard.write(keymap[fingertip_state]);
+          }
+         
+          Keyboard.println(analogRead(0));
+          Keyboard.println(fingertip_state);
+     }
+}
